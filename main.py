4d348c0cacf8e0ecda4822b08f077c7e3cb89d7c
Aleksey <z4481787@gmail.com>
2023-02-16 19:28:12 +0300

    image_promotion

--- main.py
+++ main.py
@@ -45,12 +45,32 @@ def promotion_image():
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <meta name="viewport" content="width=device-width, initial-scale=1">
+                            <link rel="stylesheet" 
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                            <link rel = stylesheet type="text/css" href="{url_for('static',
+                                                                                  filename='css/style.css')}" />
                           </head>
                           <body>
                             <h1>Жди нас Марс!</h1>
                             <img src="/static/img/mars.png" alt="здесь должна была быть картинка, но не нашлась">
-                            {'</br>'.join(text)}
+                            <div class="alert alert-dark" role="alert">
+                              Человечество вырастет из детства.
+                            </div>
+                            <div class="alert alert-success" role="alert">
+                              Человечеству мала планета.
+                            </div>
+                            <div class="alert alert-dark" role="alert">
+                              Мы сделаем обитаемыми безжиненные пока планеты.
+                            </div>
+                            <div class="alert alert-warning" role="alert">
+                              И начнём с Марса!
+                            </div>
+                            <div class="alert alert-danger" role="alert">
+                              Присоединяйся!
+                            </div>
                           </body>
                         </html>"""
 
